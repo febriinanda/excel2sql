cac2a718308aee38bd57181c8c33f65e7d7c461a
Istigfar Rahman <istigfar9237@gmail.com>
2021-10-17 11:30:55 +0700

    convert unit test ready

--- utility.py
+++ utility.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+tz_def = "19700101"
+
 
 def convert(data):
     return datetime.strptime(data, '%Y%m%d').strftime('%d-%b-%y')
@@ -11,3 +13,10 @@ def reverse(data, def_value):
 
     data.zfill(8)
     return datetime.strptime(data, '%d%m%Y').strftime('%d-%b-%y')
+
+
+def tz_checking(data):
+    if int(data) == 0:
+        return tz_def
+    else:
+        return data
